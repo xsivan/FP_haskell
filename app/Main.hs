b0feb3152a71e937c118707500b910c87ca80d5e
Luxo <74666305+luxao@users.noreply.github.com>
2022-05-02 10:43:34 +0200

    Add files via upload
    initialize

---
+++ app/Main.hs
@@ -0,0 +1,30 @@
+module Main where
+import System.IO
+import Data.List
+import Data.Char (isAlpha, toLower, isSpace)
+import qualified Data.Set
+import qualified Data.Map as Map 
+import qualified Stem 
+
+
+toWords :: String -> [String] -> [String]
+toWords s list = filter (\w -> w `notElem` list) (words s)
+
+
+removeStopWords :: IO [String]
+removeStopWords = do  
+            putStrLn "nacitaj file"   
+            handle <- readFile "app/stopwords.txt" 
+            file <- getLine
+            hndl <- readFile file
+            return (toWords hndl (lines handle))
+
+
+removeStem :: [String] -> [String]
+removeStem xs = map Stem.stem xs
+
+main :: IO()
+main = do
+        tmp <- removeStopWords
+        print (removeStem tmp)
+
