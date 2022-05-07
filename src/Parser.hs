96ac075f0648706ce3e819efd249d5d933129a12
Pavol Švidraň <pavolsvidran@gmail.com>
2022-05-07 17:59:25 +0200

    Added TODOs for work on this version
    - removed TODOs extension, it doesnt work very well in .hs files

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,4 +1,5 @@
-module Parser where
+module Parser(main, parseFiles) where
+    import System.Directory
     import System.IO
     
     main :: IO ()
@@ -7,7 +8,15 @@ module Parser where
     parseFile :: String -> String -> IO ()
     parseFile src dest = 
         putStrLn "Called parse file"
+        -- Parse name from file
+        -- Save into dest + fileName some rng words for now
 
     parseFiles :: String -> String -> IO ()
     parseFiles src dest = 
         putStrLn "Called parse files"
+        -- Check if exist source location, if no exit with error message
+        -- Check if exist target destination, if no create it
+            -- if creation fails exit with error message
+        -- Loop html files in source location, if there is no file exit with message
+            -- Per file call parseFile func
+            -- Try to implement progress-er into console, ideal with content update instead of append
